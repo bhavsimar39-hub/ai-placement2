52f2c5854fa4628dd6321647f800b9eb2705f85c
unknown <padma.24bsa10137@vitbhopal.ac.in>
2026-04-05 21:37:39 +0530

    changes

--- frontend/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-// TEMPORARY UI MESSAGES (Backend will connect in Phase 4)
-
-function loginUser() {
-    alert("Login function triggered. Backend API will be connected in later phase.");
-}
-
-function signupUser() {
-    alert("Signup function triggered. Backend API will be connected in later phase.");
-}
